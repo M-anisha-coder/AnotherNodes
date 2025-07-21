7a8e7ef0a649cb2ade71a04a9fc5dc549c934bda
Manisha <manishavisvkrama12345@gmail.com>
2025-07-21 18:18:50 +0000

    modified

--- NodeEcho/server.js
+++ NodeEcho/server.js
@@ -8,12 +8,21 @@ import {AuthorName,createFile} from './Utilities/FileSystem.js';
 var Name =AuthorName;
 createFile("Hello "+ Name);
 
-app.get("/",(req,res)=>{
-    res.send("Starting NodeEcho with"+Name);
+app.get("/api/v1/",(req,res)=>{
+    res.send("TODO-LIST WorkShop");
+});
+
+app.get("/api/v1/TodoList",(req,res)=>{
+    var todo={
+        Title:"Docker Workshop",
+        Content :"Learn building about images.",
+        keyword:["docker","Images","Containers"]
+    }
+    res.json(todo);
 });
 
 app.listen(port,()=>{
-    console.log(`server is running at https://localhost:${port}`);
+    console.log(`server is running at https://localhost:${port}/api/v1`);
 });
 
 
