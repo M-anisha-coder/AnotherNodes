3dc2f67d9ccdc930907d4e5de72dd0ac87d19078
Manisha <manishavisvkrama12345@gmail.com>
2025-07-21 11:39:51 +0000

    FileSystem, ESM and export-import

--- NodeEcho/server.js
+++ NodeEcho/server.js
@@ -4,10 +4,17 @@ const app =express();
 dotenv.config();
 const port=process.env.PORT || 3000;
 
+import {AuthorName,createFile} from './Utilities/FileSystem.js';
+var Name =AuthorName;
+createFile("Hello "+Name);
+
 app.get("/",(req,res)=>{
-    res.send("Starting NodeEcho");
+    res.send("Starting NodeEcho with"+Name);
 });
+
 
 app.listen(port,()=>{
     console.log(`server is running at https://localhost:${port}`);
 });
+
+
