fbd922d5829a15f17c9d91c81cdc97594022ac1f
Manisha <manishavisvkrama12345@gmail.com>
2025-07-23 10:38:00 +0000

    DB connection done

--- NodeEcho/src/DB/db.js
+++ NodeEcho/src/DB/db.js
@@ -1,26 +1,27 @@
 import mongoose from 'mongoose';
-import {DBName} from '../constant.js'
+import { DBNAME } from '../constant.js';
 
-if (!process.env.MONGODB_URI ) {
+if (!process.env.MONGODBURL ) {
     throw new Error("Missing MONGODBURL in environment variables");
 }
-if (!DBName ) {
+if (!DBNAME ) {
     throw new Error("Missing DBNAME in Constant.js");
 }
-const DB =mongoose.connection;
+
+const DB = mongoose.connection;
 DB.on('connected', () => console.log('✅ Connected to MongoDB server'));
 DB.on('error', (err) => console.log('❌ MongoDB connection error:', err));
 DB.on('disconnected', () => console.log('🔌 Disconnected from MongoDB server'));  
 
-const MongoDBconnection =  async()=>{
- try {
-   const connInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DBName}`);
-   console.log(`MongoDB Connected !! DB HOST : ${connInstance.connection.host}`);
-    
- } catch (error) {
+const connectDB = async () => {
+    try {
+        const connInstance= await mongoose.connect(`${process.env.MONGODBURL}/${DBNAME}`);
+        console.log(`MongoDB Connected !! DB HOST : ${connInstance.connection.host}`);
+    }
+    catch (err) {
         console.error("Failed to connect to MongoDB:", err.message);
         process.exit(1);
- }
+    }
 }
 
-export default MongoDBconnection;
+export default connectDB;
--- NodeEcho/src/server.js
+++ NodeEcho/src/server.js
@@ -1,34 +1,99 @@
-import express from 'express';
-import dotenv from 'dotenv';
-//import {DB} from './DB/db.js';
-const app =express();
-dotenv.config();
-const port=process.env.PORT || 3000;
+import './config.js';
+// import express from 'express';
+// const app = express();
+// const port = process.env.PORT || 3000;
 
-// export-import example
-// import {AuthorName,createFile} from './Utilities/FileSystem.js';
-// var Name =AuthorName;
-// createFile("Hello "+ Name);
+//------Method 1 Use DB.js File and import it
+import connectDB from"./DB/db.js";
+connectDB();
+
+//------Method 2 To connecting with DB-------------------
+
+// if (!process.env.MONGODBURL) {
+//     throw new Error("Missing MONGODBURL in environment variables");
+// }
+// const DB = mongoose.connection;
+// DB.on('connected', () => console.log('✅ Connected to MongoDB server'));
+// DB.on('error', (err) => console.log('❌ MongoDB connection error:', err));
+// DB.on('disconnected', () => console.log('🔌 Disconnected from MongoDB server'));    
+// (async () => {
+//     try {
+//         //Connecting with mongodb
+//         await mongoose.connect(`${process.env.MONGODBURL}/${DBNAME}`);
+
+//         app.on("error", (err) => {
+//             console.error("Server is not able to connect with database error :", err);
+//         });
+
+//         app.listen(port, () => {
+//             console.log(`server is running at https://localhost:${port}/api/v1`);
+//         });
+//     }
+//     catch (err) {
+//         console.error("Failed to connect to MongoDB:", err.message);
+//         process.exit(1);
+//     }
+
+// })();
+//-----------------
 
 
 
 
 
-app.get("/api/v1/",(req,res)=>{
-    res.send("TODO-LIST WorkShop");
-});
 
-app.get("/api/v1/TodoList",(req,res)=>{
-    var todo={
-        Title:"Docker Workshop",
-        Content :"Learn building about images.",
-        keyword:["docker","Images","Containers"]
-    }
-    res.json(todo);
-});
 
-app.listen(port,()=>{
-    console.log(`server is running at https://localhost:${port}/api/v1`);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get("/api/v1/",(req,res)=>{
+//     res.send("TODO-LIST WorkShop");
+// });
+
+// app.get("/api/v1/TodoList",(req,res)=>{
+//     var todo={
+//         Title:"Docker Workshop",
+//         Content :"Learn building about images.",
+//         keyword:["docker","Images","Containers"]
+//     }
+//     res.json(todo);
+// });
+
+// app.listen(port,()=>{
+//     console.log(`server is running at https://localhost:${port}/api/v1`);
+// });
 
 
